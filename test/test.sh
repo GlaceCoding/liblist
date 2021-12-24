ec596f9acd368ef525348e9fef853d129af02203
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2021-12-24 15:58:24 +0100

    Don't add list_extra_pushswap

--- test/test.sh
+++ test/test.sh
@@ -7,5 +7,5 @@ if [[ $? == 0 ]]; then
 	echo "========= LIST ========="
 	gcc -fsanitize=address -g ./test/test.c ./srcs/*.c -I "$HEAD" && ./a.out && rm ./a.out
 	echo "========================"
-	norminette ./srcs/*.c ./includes/*.h ./extra/*.c ./extra/*.h list.c list.h | grep -v ": OK!"
+	norminette ./srcs/*.c ./includes/*.h list.c list.h | grep -v ": OK!"
 fi
